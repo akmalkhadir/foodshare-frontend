0fe81f4d8b5fc1bd04306ef92142c11eac16ed50
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-30 12:50:57 +0000

    Landing Page Skeleton

---
+++ src/LandingScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const LandingScreen = () => {
+  return (
+
+    <div>
+      <div className='ui fluid container'>
+        <h2 className='ui centered header'>Share your food App!</h2>
+        <h3 className='ui centered header'>
+        Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.
+        </h3>
+        <div class='ui buttons'>
+          <button class='ui primary button'>Share Food</button>
+          <div class='or' />
+          <button class='ui primary button'>Find food to Reserve</button>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default LandingScreen
