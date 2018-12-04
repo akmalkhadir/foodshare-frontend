d5fdc9ce76c2ec7969716901f7df4c80e4686ee9
Irfan Suleman <ivsuleman@gmail.com>
2018-12-04 13:51:37 +0000

    fetch from api completed

--- src/App.js
+++ src/App.js
@@ -8,32 +8,40 @@ class App extends Component {
 
   state = {
     foods: [
-      {
-        id: 1,
-        name: 'cake',
-        description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
-        url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
-      },
-      {
-        id: 2,
-        name: 'cake',
-        description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
-        url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
-      },
-      {
-        id: 3,
-        name: 'cake',
-        description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
-        url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
-      },
-      {
-        id: 4,
-        name: 'cake',
-        description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
-        url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
-      }
+      // {
+      //   id: 1,
+      //   name: 'cake',
+      //   description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
+      //   url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
+      // },
+      // {
+      //   id: 2,
+      //   name: 'cake',
+      //   description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
+      //   url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
+      // },
+      // {
+      //   id: 3,
+      //   name: 'cake',
+      //   description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
+      //   url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
+      // },
+      // {
+      //   id: 4,
+      //   name: 'cake',
+      //   description: 'cake cake cake cake cake cake Carrot cake chupa chups sweet chocolate biscuit gummies dessert candy halvah. Muffin tiramisu lollipop jelly beans wafer sweet roll pie candy canes. Lemon drops danish marshmallow soufflé ice cream cake.',
+      //   url: 'https://www.caracaschronicles.com/wp-content/uploads/2013/03/16714-birthday-cake-760x580.jpg'
+      // }
     ],
     selectedFood: null
+  }
+
+  url = 'http://localhost:3000/v1/'
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/v1/foods`)
+      .then(resp => resp.json())
+      .then(foods => this.setState({foods}))
   }
 
   createFood = () => {
