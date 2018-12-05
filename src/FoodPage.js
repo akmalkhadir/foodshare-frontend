69b76f6bd178fc8ef23b831173d0017d3caf6279
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-05 13:33:35 +0000

    reservations list display

--- src/FoodPage.js
+++ src/FoodPage.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
+import { CardGroup, Header } from 'semantic-ui-react'
+
 import FoodList from './FoodList'
 import ReserveFoodPage from './ReserveFoodPage'
+import ReservedFoodContainer from './ReservedFoodContainer'
 
 class FoodPage extends Component {
   render () {
-    const { foods, foodCardClickHandler, selectedFood } = this.props
+    const { foods, foodCardClickHandler, selectedFood, reservedFood } = this.props
     return (
       <div>
         { selectedFood === null
-          ? <FoodList
-            foods={foods}
-            foodCardClickHandler={foodCardClickHandler}
-          />
+          ? <CardGroup>
+            <FoodList
+              foods={foods}
+              foodCardClickHandler={foodCardClickHandler}
+            />
+          </CardGroup>
           : <ReserveFoodPage selectedFood={selectedFood} />
         }
+        <Header as='h3' block>
+          Your Reserved Foods
+        </Header>
+        <CardGroup>
+          <ReservedFoodContainer foods={reservedFood} />
+        </CardGroup>
       </div>
     )
   }
